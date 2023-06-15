887f2cd11673f692b8ed78a7409bd98db981c3c4
rauda08 <raudajnnh08@gmail.com>
2023-06-15 15:07:45 +0700

    menambahkan element

--- stackarray/stackarray.cpp
+++ stackarray/stackarray.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include<string>
-- using namespace std;
+using namespace std;
 
 class StackArray {
 private: 
-	string stack_array[5];
+	string Stack_array[5];
 	int top;
 public:
 	//constuctor
 	StackArray() {
 		top = -1;
 	}
+	string push(string element) {
+		if (top == 4) { //step 1
+			cout << "Number of data exceeds the limit," << endl;
+			return "";
